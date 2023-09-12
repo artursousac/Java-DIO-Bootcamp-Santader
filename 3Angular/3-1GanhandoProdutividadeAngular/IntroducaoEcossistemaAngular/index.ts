8876b1935142c9649bd9b0b91c1cdcd78e9fca3b
Artur Cavalcante <artursousac@hotmail.com>
2023-09-12 12:44:45 -0300

    concluido modulo de introducao ao ecossistema angular

---
+++ 3Angular/3-1GanhandoProdutividadeAngular/IntroducaoEcossistemaAngular/index.ts
@@ -0,0 +1,27 @@
+/*
+    Framework baseado em Javascript, possui Typescript como linguagem principal.
+    SPAs (Single page aplications) são o seu foco 
+    Componentes são a base da arquitetura
+    Estrutura organizada e bem definida
+    Sistema de Tooling (Ferramentas) rico: CLI, HTTP, Router...
+
+    Pré-Requisitos para angular é HTML e CSS, Node e NPM, JavaScript e TypeScript
+
+    React é mais uma biblioteca e não vai impor algo, deixando o projeto mais solto.
+    Angular é algo mais definido e infere de como deve programar e só vai precisar colocar o quebra-cabeça, sendo tudo bem estruturado. Em contrapartida o Setup é maior.
+
+
+    Camadas de WebApp:
+    - Componentes, Gerenciamento de Estado, Roteamento, Renderização.
+
+    -> Componentes: é um elemento visual, customizável e reutilizável.
+    -> Gerenciamento de Estados: Responsável por garantir que as informações dos componentes não estão dessincronizadas
+        e facilitar a comunicação de um componente com o outro.
+    -> Roteamento: Responsável pela forma de navegar fazendo a troca de URL sem recarregar a página, mudando somente os componentes
+        que são mostrados
+    -> Renderização: Decide a melhor maneira de acessar e entregar para o browser o componente pronto para que ele seja desenhado
+        na janela sem compilações
+
+    
+
+*/
